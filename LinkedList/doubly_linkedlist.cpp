17a5aa3560d3a1231a07cd6cc55472df76b6b4cd
DivyanshuKaushik <divyanshukaushik33@gmail.com>
2022-06-10 19:43:14 +0530

    insertion in dll

--- LinkedList/doubly_linkedlist.cpp
+++ LinkedList/doubly_linkedlist.cpp
@@ -2,8 +2,61 @@
 
 using namespace std;
 
-int main(){
-    
+class Node{
+    public:
+        int data;
+        Node *prev;
+        Node *next;
+        Node(int data){
+            this->data = data;
+            this->prev = NULL;
+            this->next = NULL;
+        }
+};
 
+void display(Node* head){
+    Node* temp = head;
+    while(temp!=NULL){
+        int prev = temp->prev ? temp->prev->data : 0;
+        cout<<"prev:"<<prev<<"value:"<<temp->data<<endl;
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+
+void insertAtHead(Node* &head,int data){
+    // create new node 
+    Node* newNode = new Node(data);
+    newNode->next = head;
+    // head->prev = newNode;
+    head=newNode;
+}
+
+void insertAtTail(Node* &head,int data){
+    if(head==NULL){
+        insertAtHead(head,data);
+        return;
+    }
+    // create new node 
+    Node* newNode = new Node(data);
+    Node* temp = head;
+    while(temp->next!=NULL){
+        temp = temp->next;
+    }
+    newNode->prev = temp;
+    temp->next = newNode;
+    // head = newNode;
+}
+
+
+
+int main(){
+
+    Node* head = NULL;
+    insertAtHead(head,10);
+    insertAtTail(head,20);
+    insertAtTail(head,30);
+
+    display(head);
     return 0;
 }
---
+++ LinkedList/tempCodeRunnerFile.cpp
@@ -0,0 +1,2 @@
+tTail(head,20);
+    insertAtTail(head,30);
