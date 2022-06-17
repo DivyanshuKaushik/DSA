6df9049471b05466b176f64f10929a715a0a973b
DivyanshuKaushik <divyanshukaushik33@gmail.com>
2022-06-17 18:09:17 +0530

    append k node ll

---
+++ LinkedList/append_last_k_nodes.cpp
@@ -0,0 +1,23 @@
+/** 
+Append k last node of a linked list 
+
+LL -> 1 2 3 4 5 6
+
+k = 3
+
+******* Solution *******
+
+LL -> 4 5 6 1 2 3 
+*/
+
+#include<bits/stdc++.h>
+// use functions linkedlist.cpp 
+#include "linkedlist.cpp"
+
+using namespace std;
+
+int main(){
+    
+
+    return 0;
+}
